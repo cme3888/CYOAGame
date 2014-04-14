1e9757917a47e0565972cc2c7f84429842cf868a
Chelsie Erickson <cme3888@chelsies-air.gateway.2wire.net>
2014-04-13 20:46:59 -0400

    first commit

---
+++ game.js
@@ -0,0 +1,38 @@
+<script>
+  alert('Welcome to Ahh! Zombies!');
+</script>
+<script>
+    var name = prompt(" What is your name?");
+    var message = 'Hello, ' + name + ' you are the last survivor!';
+    alert(message)
+</script>
+<script>
+    alert("Choose your own Adventure! Click OK to Survive!");
+</script>
+<script>
+    alert("It\'s a beautiful day and you are walking around the IronYard thinking about the mountains of homework you have been assigned not even aware of your surroundings.");
+    alert("You look up and notice that the other students are looking a little under the weather but you brush it off.");
+    alert("Then out of the corner of your eye, you see a hand reaching out to you.");
+ </script>   
+<script>
+alert("You turn around to see....A ZOMBIE! It\'s about to attack!");
+var answer = prompt("What do you do? Type 1 for Run Fast, Type 2 for Fight, Type 3 for Breakdance");
+if(answer==="1"){
+   alert("You Run, far and fast and he can\'t keep up");
+   alert("You beat the game!");
+}
+else if(answer==="2"){
+   alert("You are noble and stayed to fight");
+   alert("Zombie infects you and eats your brains");
+   alert("You lost the game");
+}
+else if(answer==="3"){
+   alert("You breakdance");
+   alert("the zombie also likes to dance");
+   confirm("You continue to Survive");
+</script>
+
+
+
+
+   
